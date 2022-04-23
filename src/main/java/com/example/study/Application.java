e47637b7c9a55d59eeef2073b922327ac0035715
Coding-HHH <420387945@qq.com>
2022-04-23 23:14:00 +0800

    github的令牌token问题处理

--- src/main/java/com/example/study/Application.java
+++ src/main/java/com/example/study/Application.java
@@ -9,6 +9,13 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ *  参考； https://zhuanlan.zhihu.com/p/414028184
+ *  github的token：  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
+ *  把token添加远程仓库链接中：
+ *  git remote set-url origin https://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@github.com/Coding-HHH/springboot-study.git/
+ *
+ */
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @MapperScan({"com.example.redis.*"})
@@ -17,7 +24,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableEncryptableProperties
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
